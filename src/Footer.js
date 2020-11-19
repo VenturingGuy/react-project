dc20ab60f836cc8764d2e55f6f17dae35eaaf910
Luis <LVentura335@Gmail.com>
2020-11-18 17:48:45 -0800

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+// src/Footer.js
+
+import React from 'react'
+import logo from './logo.svg'
+
+function Footer() {
+    return (
+        <footer>Luis Ventura Copyright 2020</footer>
+    )
+}
+
+export default Footer
