8873d436879af3cd6ba13d5b301830097cc05e5b
Luis <LVentura335@Gmail.com>
2020-11-18 16:08:09 -0800

    added images; js files

---
+++ src/POPOSSpace.js
@@ -0,0 +1,20 @@
+// src/POPOSSpace.js
+
+import React from 'react'
+import logo from './logo.svg'
+
+function POPOSSpace() {
+    return (
+        <div>
+            <img src={`${process.env.PUBLIC_URL}images/50-california-st.jpg`} 
+            width="300"
+            height="300"
+            alt="50 California St."
+            />
+            <h1>Name...</h1>
+            <div>Address...</div>
+        </div>
+    )
+}
+
+export default POPOSSpace
