d25d8360be9759b8ce1c99e2c8bb4585d8f2a66d
Luis <LVentura335@Gmail.com>
2020-11-20 06:55:12 -0800

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+// src/Footer.js
+
+import React from 'react'
+import logo from './logo.svg'
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="Footer">Luis Ventura Copyright 2020</footer>
+    )
+}
+
+export default Footer
