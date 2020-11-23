3cc59ca8390f0c1aeff2beaf5b09cdfbab8a64c2
Luis <LVentura335@Gmail.com>
2020-11-22 18:19:38 -0800

    added space styling

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -12,11 +12,15 @@ function POPOSSpace(props) {
             <Link to={`/details/${id}`}>
               <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
             </Link>
-            <Link to={`/details/${id}`}>
+            <Link
+              className="POPOSSPace-title" // add a class name!
+              to={`/details/${id}`}>
               <h1>{name}</h1>
             </Link>
-            <div>{address}</div>
-            <div id="hours">{hours}</div>
+            <div className="POPOSSpace-info">
+              <div>{address}</div>
+              <div id="hours">{hours}</div>
+            </div>
         </div>
     )
 }
