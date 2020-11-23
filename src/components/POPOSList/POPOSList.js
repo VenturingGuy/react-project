16611fd2292515658a0df51d388342118d956cb0
Luis <LVentura335@Gmail.com>
2020-11-22 21:18:04 -0800

    added search functionality

--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,26 +1,32 @@
 // src/POPOSList.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import data from '../../sfpopos-data.json'
 import POPOSSpace from '../POPOSSpace/POPOSSpace';
 import './POPOSList.css';
 
 function POPOSList() {
-
-    const spaces = data.map(( { title, address, images, hours }, i) => {
-        return (
-            <POPOSSpace
-              id={i}
-              key={title}
-              name={title}
-              address={address}
-              image={images[0]}
-              hours={hours}
-            />
-        )
+  const [ query, setQuery ] = useState('')
+  const spaces = data.filter(obj => obj.title.includes(query) ||
+    obj.address.includes(query)).map((obj, i) => {
+      return (
+        <POPOSSpace
+          name={obj.title}
+          address={obj.address}
+          image={obj.images[0]}
+        />
+      )
     })
     return (
         <div className="POPOSList">
+          <form>
+            <input
+              value={query}
+              placeholder="search"
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button type="submit">Submit</button>
+          </form>
           { spaces }
         </div>
     )
